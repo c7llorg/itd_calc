86cd6a0032ef49444683b916ec08ba17e9295a90
Michael_Konev <pvz.g@list.ru>
2026-03-18 20:46:01 +0300

    Решил вспомнить и усложнить общую схему работы... Основная работа происходит в функциях Реализовывать задачи в функции process_calculation

--- calculator.cpp
+++ calculator.cpp
@@ -3,5 +3,17 @@
 
 int main() {
 
+    double var_first = 0.0, var_second = 0.0;
+    char operation = '/';
+
+    std::cout << "Добро пожаловать в калькулятор!" << std::endl
+    << "Выполнено Коневом Михаилом и Данилкиной Елизаветой, ИТД-11М" << std::endl
+    << "Введите первое число: ";
+    std::cin >> var_first;
+    std::cout << std::endl << "Введите второе число: ";
+    std::cin >> var_second;
+    std::cout << std::endl << "Выберите операцию (+ - * /):";
+    std::cin >> operation;
+    std::cout << "Testing: " << var_first << var_second << operation;
     return 0;
 }
